d43da04de6f8909191e50a4ea1049fee12f72874
Gareth Jones <garethjns4@gmail.com>
2020-03-24 22:28:18 +0000

    Add publish workflow

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ import setuptools
 from social_distancing_sim import __version__
 
 
+"""
 with open("README.md", "r") as fh:
     long_description = fh.read()
+"""
 
 setuptools.setup(
-    name="SocialDistancingSim",  # Replace with your own username
+    name="social_distancing_sim",
     version=__version__,
     author="Gareth Jones",
     author_email="",
     description="",
-    long_description=long_description,
+    long_description='',
     long_description_content_type="text/markdown",
     url="https://github.com/garethjns/social-distancing-sim",
     packages=setuptools.find_packages(),
